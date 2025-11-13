a77e4bd09e75fc0ba6f59217ad727d345ea6054d
Konstantin Melnikov <k.melnikov@primo-rpa.ru>
2025-11-13 09:01:08 +0300

    task 2

--- task_2/main.go
+++ task_2/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
-	// TODO
+import "fmt"
+
+func test() (x int) {
+	defer func() {
+		x++
+	}()
+	x = 1
+	return
 }
+
+func anotherTest() int {
+	var x int
+	defer func() {
+		x++
+	}()
+	x = 1
+	return x
+}
+
+func main() {
+	fmt.Println(test())
+	fmt.Println(anotherTest())
+}
+
+/*
+defer'ы выполнятся в порядке запуков функций
+в первом выведется 2, потому что x - именнованное возвращаемое значение
+во втором 1, так как возвращается неименованное значение
+*/
