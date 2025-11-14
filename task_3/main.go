ad4228130c2fe3143c7902cd352796cc1142787b
Konstantin Melnikov <k.melnikov@primo-rpa.ru>
2025-11-14 21:54:04 +0300

    task 3

--- task_3/main.go
+++ task_3/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
-	// TODO
+import (
+	"fmt"
+	"os"
+)
+
+func Foo() error {
+	var err *os.PathError = nil
+	return err
 }
+
+func main() {
+	err := Foo()
+	fmt.Println(err)
+	fmt.Println(err == nil)
+}
+
+/*
+Интерфейс - под капотом, это структура с двумя полями: указатель на тип и указатель на данные.
+Интерфейс равен nil только тогда, когда оба этих указателя равны nil.
+В свою ч
+очередь, в пустом интерфейсе (interface{}) нет указателя на тип, поэтому он равен nil, когда указатель на данные равен nil.
+*/
