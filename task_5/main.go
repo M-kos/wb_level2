48d28bce3f875a86f21cb6ae59e590edb773f1df
Konstantin Melnikov <k.melnikov@primo-rpa.ru>
2025-11-14 21:54:16 +0300

    task 5

--- task_5/main.go
+++ task_5/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
-	// TODO
+type customError struct {
+	msg string
 }
+
+func (e *customError) Error() string {
+	return e.msg
+}
+
+func test() *customError {
+	// ... do something
+	return nil
+}
+
+func main() {
+	var err error
+	err = test()
+	if err != nil {
+		println("error")
+		return
+	}
+	println("ok")
+}
+
+/*
+Опять про интерфейс. Функция test возвращает значение с конкретным типом,
+а значит переменная err с интерфейсом error не может быть равна nil.(интерфейс равен nil, когда и значение и тип равны nil)
+*/
