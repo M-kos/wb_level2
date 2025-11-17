e67815e6db14263ae6c5b96c679d5e239872919c
Konstantin Melnikov <k.melnikov@primo-rpa.ru>
2025-11-17 18:43:22 +0300

    task 11

--- task_11/main.go
+++ task_11/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 func main() {
-	// TODO
+	fmt.Println(anagramms([]string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик", "стол"}))
+}
+
+func anagramms(input []string) map[string][]string {
+	temp := make(map[string]string)
+	result := make(map[string][]string)
+
+	for _, v := range input {
+		ordered := sortChars(v)
+		lowV := strings.ToLower(v)
+
+		if str, ok := temp[ordered]; ok {
+			if _, ok := result[str]; ok {
+				result[str] = append(result[str], lowV)
+				continue
+			}
+
+			result[str] = append(result[str], str, lowV)
+			continue
+		}
+
+		temp[ordered] = lowV
+	}
+
+	return result
+}
+
+func sortChars(s string) string {
+	chars := strings.Split(s, "")
+	sort.Strings(chars)
+	return strings.Join(chars, "")
 }
